a9b4bb98b62c2bc59f0d35c528fa8d8834d37d1c
hasnainn135 <165057951+hasnainn135@users.noreply.github.com>
2025-03-08 03:05:23 +0000

    completed front-end Register page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -18,6 +18,31 @@ const register = () => {
                 <button onClick={()=> setSelectedRole("student")}  type="button" className={`w-full rounded-md font-medium ${selectedRole === "student"? "bg-green-700 text-white":"bg-transparent text-green-700"} h-full`}>Student</button>
                 <button onClick={()=> setSelectedRole("tutor")}  type="button" className={`w-full rounded-md font-medium ${selectedRole === "tutor"? "bg-green-700 text-white":"bg-transparent text-green-700"} h-full`}>Tutor</button>
             </div>
+            <form className="w-[30rem] mt-3 space-y-2">
+                <div className='flex flex-row w-full'>
+                    <div className='flex flex-col mr-2 w-full'>
+                    <label htmlFor="username">User name <span className='text-red-600'>*</span></label>
+                    <input type="text" id='username' name='username' className='border border-[#bababa]  rounded-md mt-2 h-9 ' />
+                    </div>
+                    <div className='flex flex-col w-full'>
+                    <label htmlFor="email">Email <span className='text-black'>*</span></label>
+                    <input type="email" id='email' name='email' className='border border-[#bababa]  rounded-md mt-2 h-9' />
+                    </div> 
+                </div>
+                
+                <div className='flex flex-row pb-2'> 
+                    <div className='flex flex-col mr-2 w-full'>
+                    <label htmlFor="password">Password <span className='text-red-600'>*</span></label>
+                    <input type="password" id='password' name='password' className='border border-[#bababa]  rounded-md mt-2 h-9' />
+                    </div>
+                    <div className='flex flex-col w-full'>
+                    <label htmlFor="conf_passowrd">Confirm Password <span className='text-red-600'>*</span></label>
+                    <input type="password" id='conf_password' name='conf_password' className='border border-[#bababa]  rounded-md mt-2 h-9' />
+                    </div>
+                </div>
+                <button type="submit" className="bg-[#169962] border w-full h-9 border-[#bababa] rounded-md text-white py-1">Register</button>
+                <p className='text-green-700 text-center'>Already have an Account?</p>
+            </form>
         </div>
         
         
